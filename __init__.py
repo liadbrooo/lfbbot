b31aab28ef8a692c419fcc0936bc38688bcc3d2b
liadbrooo <liadbro@icloud.com>
2026-02-15 02:51:58 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-LFBBotTicketTool - Umfassendes Ticket-System für Red-DiscordBot
-Version: 1.0.0
-"""
-
-from .LFBBotTicketTool import LFBBotTicketTool
-
-__all__ = ["LFBBotTicketTool"]
-__version__ = "1.0.0"
